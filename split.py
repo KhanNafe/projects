7188eee070ec399393d7d27929d15eaa139fc994
KhanNafe <nafekhan54@gmail.com>
2025-02-28 14:31:42 +0530

    Delete split.py

--- split.py
+++
@@ -1,9 +0,0 @@
-def count_word(sentence):
-    words=sentence.split()
-    count=0
-    for word in words:
-        count+=1
-    return count
-#test the function
-sentence="I like Apples"
-print("word count=",count_word(sentence))
